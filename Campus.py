479d3a0cbf80dd88922b3ef81d73b45c39cd21f4
Jack21Ace <djherrera89639@umanizales.edu.co>
2021-09-12 20:33:20 -0500

    Use, Supplier, Enums, Campus OK

---
+++ Campus.py
@@ -0,0 +1,109 @@
+# Import de las clases dependientes
+from typing import Any
+#import Item
+#import Bill
+#import Payroll
+
+# Imports
+import Supplier
+
+# Creacion de clase Campus
+class Campus:
+    # Instancia del constructor "__init__"
+    def __init__(self, __nit:int, __name:str, __phone:str, __address:str, __parking:bool, __sizeParking:int,
+                __inventory:Any, __technicalEmpl:bool, __supplier:Supplier, __bill:Any, __payroll:Any):
+        self.nit = __nit
+        self.name = __name
+        self.phone = __phone
+        self.address = __address
+        self.parking = __parking
+        self.sizeParking = __sizeParking
+        self.inventory = __inventory
+        self.technicalEmpl = __technicalEmpl
+        self.supplier = __supplier
+        self.bill = __bill
+        self.payroll = __payroll
+
+# getter && setter STARTS
+    # Getter para nit
+    def getNit(self):
+        return self.nit
+
+    # Getter && Setter para name
+    def getName(self):
+        return self.name
+
+    def setName(self, __name):
+        self.name = __name
+
+    # Getter && Setter para phone
+    def getPhone(self):
+        return self.phone
+
+    def setPhone(self, __phone):
+        self.phone = __phone
+
+    # Getter && Setter para address
+    def getAddress(self):
+        return self.address
+
+    def setAddress(self, __address):
+        self.address = __address
+
+    # Getter && Setter para parking
+    def getParking(self):
+        return self.parking
+
+    def setParking(self, __parking):
+        self.parking = __parking
+
+    # Getter && Setter para sizeParking
+    def getSizeParking(self):
+        return self.sizeParking
+
+    def setSizeParking(self, __sizeParking):
+        self.sizeParking = __sizeParking
+
+    # Getter && Setter para inventory
+    def getInventory(self):
+        return self.inventory
+
+    def setInventory(self, __inventory):
+        self.inventory = __inventory
+
+    # Getter && Setter para technicalEmpl
+    def getTechnicalEmpl(self):
+        return self.technicalEmpl
+
+    def setTechnicalEmpl(self, __technicalEmpl):
+        self.technicalEmpl = __technicalEmpl
+
+    # Getter && Setter para supplier
+    def getSupplier(self):
+        return self.supplier
+
+    def setSupplier(self, __supplier):
+        self.supplier = __supplier
+
+    # Getter && Setter para bill
+    def getBill(self):
+        return self.bill
+
+    def setBill(self, __bill):
+        self.bill = __bill
+
+    # Getter && Setter para payroll
+    def getPayroll(self):
+        return self.payroll
+
+    def setPayroll(self, __payroll):
+        self.payroll = __payroll
+# getter && setter ENDS
+
+# Aqui la instancia de la clase Campus
+campus = Campus(96348512, 'Iron GYM', '(606) 725-8569', 'KR 48 # 64-28 piso 1', True, 10, Any, False, Supplier, Any, Any)
+
+# Test
+print(f"Nombre de la sede {campus.getName()}\nY su numero de telefono es {campus.getPhone()}.")
+
+#print("Nombre de la sede: " + campus.getName() + "\nY su numero de telefono es: " + campus.getPhone())
--- User.py
+++ User.py
@@ -1,23 +1,34 @@
 from enum import Enum
 from datetime import date
+from typing import Any
 from Enums import Eps, Rh, Gender
+import datetime
+# Imports de las clases pendientes
+"""import Bill
+import RoutineHistory
+import ScheduleEmpl
+import ScheduleUser
+import MonthlyPay
+import Purpose
+import MedicalControl
+import Routine
+import RankingEmpl
+"""
 
 #Creación de la clase usuario
 class User:
     # Declaración del constructor
-    def __init__(self, __dni, __name, __lastName, __Gender, __phoneNumber,
-                __emergencyContact, __emailAddress, __password, __address,
-                __size, __weight, __vehicle, __purpose, __Rh, __Eps,
-                __birthday, __medicalControl, __routineUser, __rankingEmpl,
-                __scheduleUser, __scheduleEmpl, __routineHistory, __bill,
-                __monthlyPay):
+    def __init__(self, __dni:int, __name:str, __lastName:str, __gender:Gender, __phoneNumber:str, __emergencyContact:str,
+                __emailAddress:str, __password:str, __address:str, __size:float, __weight:float, __vehicle:bool,
+                __purpose:Any, __rh:Rh, __eps:Eps, __birthday:datetime, __medicalControl:Any,
+                __routine:Any, __rankingEmpl:Any, __scheduleUser:Any, __scheduleEmpl:Any,
+                __routineHistory:Any, __bill:Any, __monthlyPay:Any):
+
         # Datos de entrada
-        """Accepts only int"""
-        assert isinstance(__dni, int)
         self.dni = __dni
         self.name = __name
         self.lastName = __lastName
-        self.gender = __Gender
+        self.gender = __gender
         self.phoneNumber = __phoneNumber
         self.emergencyContact = __emergencyContact
         self.emailAddress = __emailAddress
@@ -27,11 +38,11 @@ class User:
         self.weight = __weight
         self.vehicle = __vehicle
         self.purpose = __purpose
-        self.rh = __Rh
-        self.eps = __Eps
+        self.rh = __rh
+        self.eps = __eps
         self.birthday = __birthday
         self.medicalControl = __medicalControl
-        self.routineUser = __routineUser
+        self.routineUser = __routine
         self.rankingEmpl = __rankingEmpl
         self.scheduleUser = __scheduleUser
         self.scheduleEmpl = __scheduleEmpl
@@ -39,17 +50,160 @@ class User:
         self.bill = __bill
         self.monthlyPay = __monthlyPay
 
+# getter && setter STARTS
+    # Getter para dni
     def getDni(self):
         return self.dni
 
+    # getter && setter para name
     def getName(self):
         return self.name
 
     def setName(self, __name):
         self.name = __name
 
-persona1 = User(1054995036,'Donald','Herrera Vargas', 'Masculino', '3012232219', '3149847223', 'jack2119hv@gmail.com', 'Chupnelohpts', 'Kr 26 # 47-15', 1.78, 78, True, 'Tonificar', Rh.O_POSITIVE, Eps.SURA, date(1994, 3, 13), "Sura", "Pecho", 'Ranking', 'Programación Usuario', 'Programación Usuario', 'Historico de rutinas', 'factura', 'mensualidades')
-print(persona1.eps)
-print(persona1.rh)
+    # getter && setter para lastName
+    def getLastName(self):
+        return self.lastName
 
+    def setLastName(self, __lastName):
+        self.lastName = __lastName
 
+    # getter && setter para Gender
+    def getGender(self):
+        return self.gender
+
+    def setGender(self, __Gender):
+        self.gender = __Gender
+
+    # getter && setter para phoneNumber
+    def getPhoneNumber(self):
+        return self.phoneNumber
+
+    def setPhoneNumber(self, __phoneNumber):
+        self.phoneNumber = __phoneNumber
+
+    # getter && setter para emergencyContact
+    def getEmergencyContact(self):
+        return self.emergencyContact
+
+    def setEmergencyContact(self, __emergencyContact):
+        self.emergencyContact = __emergencyContact
+
+    # getter && setter para password
+    def getPassword(self):
+        return self.password
+
+    def setPassword(self, __password):
+        self.password = __password
+
+    # getter && setter para address
+    def getAddress(self):
+        return self.address
+
+    def setAddress(self, __address):
+        self.address = __address
+
+    # getter && setter para size
+    def getSize(self):
+        return self.size
+
+    def setSize(self, __size):
+        self.size = __size
+
+    # getter && setter para weight
+    def getWeight(self):
+        return self.weight
+
+    def setWeight(self, __weight):
+        self.weight = __weight
+
+    # getter && setter para Purpose
+    def getPurpose(self):
+        return self.purpose
+
+    def setPurpose(self, __Purpose):
+        self.purpose = __Purpose
+
+    # getter && setter para Rh
+    def getRh(self):
+        return self.rh
+
+    def setRh(self, __Rh):
+        self.rh = __Rh
+
+    # getter && setter para Eps
+    def getEps(self):
+        return self.eps
+
+    def setEps(self, __Eps):
+        self.eps = __Eps
+
+    # getter && setter para birthday
+    def getBirthday(self):
+        return self.birthday
+
+    def setBirthday(self, __birthday):
+        self.birthday = __birthday
+
+    # getter && setter para MedicalControl
+    def getMedicalControl(self):
+        return self.medicalControl
+
+    def setMedicalControl(self, __MedicalControl):
+        self.medicalControl = __MedicalControl
+
+    # getter && setter para Routine
+    def getRoutine(self):
+        return self.routine
+
+    def setRoutine(self, __Routine):
+        self.routine = __Routine
+
+    # getter && setter para RankingEmpl
+    def getRankingEmpl(self):
+        return self.rankingEmpl
+
+    def setRankingEmpl(self, __RankingEmpl):
+        self.rankingEmpl = __RankingEmpl
+
+    # getter && setter para ScheduleUser
+    def getScheduleUser(self):
+        return self.scheduleUser
+
+    def setScheduleUser(self, __ScheduleUser):
+        self.scheduleUser = __ScheduleUser
+
+    # getter && setter para ScheduleEmpl
+    def getScheduleEmpl(self):
+        return self.scheduleEmpl
+
+    def setScheduleEmpl(self, __ScheduleEmpl):
+        self.scheduleEmpl = __ScheduleEmpl
+
+    # getter && setter para RoutineHistory
+    def getRoutineHistory(self):
+        return self.routineHistory
+
+    def setRoutineHistory(self, __RoutineHistory):
+        self.routineHistory = __RoutineHistory
+
+    # getter && setter para Bill
+    def getBill(self):
+        return self.bill
+
+    def setBill(self, __Bill):
+        self.bill = __Bill
+
+    # getter && setter para MonthlyPay
+    def getMonthlyPay(self):
+        return self.monthlyPay
+
+    def setMonthlyPay(self, __MonthlyPay):
+        self.monthlyPay = __MonthlyPay
+# getter && setters ENDS
+
+person1 = User(1054995036,'Donald','Herrera Vargas', Gender.MALE, '3012232219', '3149847223', 'jack2119hv@gmail.com', 'Chupnelohpts', 'Kr 26 # 47-15', 1.78, 78, True, 'Tonificar', Rh.O_POSITIVE, Eps.SURA, date(1994, 3, 13), "Sura", "Pecho", 'Ranking', 'Programación Usuario', 'Programación Usuario', 'Historico de rutinas', 'factura', 'mensualidades')
+
+print(f"El nombre del usuario es {person1.getName()}\n Su apellido es {person1.getLastName()}\n"+
+    f"Su genero es {person1.getGender()}")
