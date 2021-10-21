1f33f7bf57c947158cf869d889194a13c9781173
Jack21Ace <djherrera89639@umanizales.edu.co>
2021-10-21 00:10:48 -0500

    Terminada Clase User y Enumeraciones

--- User.py
+++ User.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 from Enums import Eps, Rh, Gender, Purpose, Ranking
 from datetime import date
 # Imports de las clases pendientes
@@ -15,17 +15,16 @@ import RoutineHistory
 # import RoutineHistory
 
 #Creación de la clase usuario
-class User:
+class User(object):
     # Declaración del constructor
     def __init__(self, __dni:int, __name:str, __lastName:str, __gender:Gender, __phoneNumber:str, __emergencyContact:str,
                 __emailAddress:str, __password:str, __address:str, __size:float, __weight:float, __vehicle:bool,
-                __purpose:Purpose, __rh:Rh, __eps:Eps, __birthDate:date, __bill:Any, __routine:Any,
-                __monthlyPay:Any, __monthlyPayHistory:Any, __employee:Any, __scheduleUser:Any,
-                __routineHistory:Any, __medicalControl:Any):
+                __purpose:Purpose, __rh:Rh, __eps:Eps, __birthDate:date, __bill:Any, __listBill:List[Any], __routine:Any,
+                __monthlyPay:Any, __monthlyPayHistory:List[Any], __employee:List[Any], __scheduleUser:List[Any], 
+                __routineHistory:List[Any]):
 
         # Datos de entrada
         self.dni = __dni
-
         self.name = __name
         self.lastName = __lastName
         self.gender = __gender
@@ -42,13 +41,16 @@ class User:
         self.eps = __eps
         self.birthDate = __birthDate
         self.bill = __bill
-        self.routineUser = __routine
+        self.listBill = __listBill
+        self.routine = __routine
+        self.routineHistory = __routineHistory
         self.monthlyPay = __monthlyPay
         self.monthlyPayHistory = __monthlyPayHistory
         self.employee = __employee
         self.scheduleUser = __scheduleUser
-        self.routineHistory = __routineHistory
-        self.medicalControl = __medicalControl
+        self.medicalControl = Any
+        # Hasta no ser creada la clase de Medical control, no puedo hacer la agregación.
+        #self.medicalControl = MedicalControl
 
 # getter && setter STARTS
     # Getter para dni
@@ -59,70 +61,74 @@ class User:
     def getName(self):
         return self.name
 
-    def setName(self, __name):
+    def setName(self, __name:str):
         self.name = __name
 
     # getter && setter para lastName
     def getLastName(self):
         return self.lastName
 
-    def setLastName(self, __lastName):
+    def setLastName(self, __lastName:str):
         self.lastName = __lastName
 
-    # getter && setter para Gender
+    # getter para Gender
     def getGender(self):
         return self.gender
-
-    def setGender(self, __Gender):
-        self.gender = __Gender
 
     # getter && setter para phoneNumber
     def getPhoneNumber(self):
         return self.phoneNumber
 
-    def setPhoneNumber(self, __phoneNumber):
+    def setPhoneNumber(self, __phoneNumber:str):
         self.phoneNumber = __phoneNumber
 
     # getter && setter para emergencyContact
     def getEmergencyContact(self):
         return self.emergencyContact
 
-    def setEmergencyContact(self, __emergencyContact):
+    def setEmergencyContact(self, __emergencyContact:str):
         self.emergencyContact = __emergencyContact
+
+    # getter && setter para emailAddress
+    def getEmailAddress(self):
+        return self.emailAddress
+
+    def setEmailAddress(self, __emailAddress:str):
+        self.emailAddress = __emailAddress
 
     # getter && setter para password
     def getPassword(self):
         return self.password
 
-    def setPassword(self, __password):
+    def setPassword(self, __password:str):
         self.password = __password
 
     # getter && setter para address
     def getAddress(self):
         return self.address
 
-    def setAddress(self, __address):
+    def setAddress(self, __address:str):
         self.address = __address
 
     # getter && setter para size
     def getSize(self):
         return self.size
 
-    def setSize(self, __size):
+    def setSize(self, __size:float):
         self.size = __size
 
     # getter && setter para weight
     def getWeight(self):
         return self.weight
 
-    def setWeight(self, __weight):
+    def setWeight(self, __weight:float):
         self.weight = __weight
 
     # getter && setter para Purpose
     def getPurpose(self):
         return self.purpose
 
-    def setPurpose(self, __Purpose):
+    def setPurpose(self, __Purpose:Purpose):
         self.purpose = __Purpose
 
     # getter para Rh
@@ -133,71 +139,77 @@ class User:
     def getEps(self):
         return self.eps
 
-    def setEps(self, __Eps):
+    def setEps(self, __Eps:Eps):
         self.eps = __Eps
 
     # getter para birthDate
     def getBirthDate(self):
         return self.birthDate
 
-    # getter && setter para MedicalControl
-    def getMedicalControl(self):
-        return self.medicalControl
+    #getter para Bill
+    def getBill(self):
+        return self.bill
 
-    def setMedicalControl(self, __MedicalControl):
-        self.medicalControl = __MedicalControl
+    #getter para ListBill
+    def getListBill(self):
+        return self.listBill
 
     # getter && setter para Routine
     def getRoutine(self):
         return self.routine
 
-    def setRoutine(self, __Routine):
-        self.routine = __Routine
-
-    # getter && setter para RankingEmpl
-    def getRankingEmpl(self):
-        return self.rankingEmpl
-
-    def setRankingEmpl(self, __RankingEmpl):
-        self.rankingEmpl = __RankingEmpl
-
-    # getter && setter para ScheduleUser
-    def getScheduleUser(self):
-        return self.scheduleUser
-
-    def setScheduleUser(self, __ScheduleUser):
-        self.scheduleUser = __ScheduleUser
-
-    # getter && setter para ScheduleEmpl
-    def getScheduleEmpl(self):
-        return self.scheduleEmpl
-
-    def setScheduleEmpl(self, __ScheduleEmpl):
-        self.scheduleEmpl = __ScheduleEmpl
+    def setRoutine(self, __routine:Any):
+        self.routine = __routine
 
     # getter && setter para RoutineHistory
     def getRoutineHistory(self):
         return self.routineHistory
 
-    def setRoutineHistory(self, __RoutineHistory):
-        self.routineHistory = __RoutineHistory
-
-    # getter && setter para Bill
-    def getBill(self):
-        return self.bill
-
-    def setBill(self, __Bill):
-        self.bill = __Bill
-
     # getter && setter para MonthlyPay
     def getMonthlyPay(self):
         return self.monthlyPay
 
-    def setMonthlyPay(self, __MonthlyPay):
-        self.monthlyPay = __MonthlyPay
-# getter && setters ENDS
+    # getter  para MonthlyPayHistory
+    def getMonthlyPayHistory(self):
+        return self.monthlyPayHistory
 
-# person1 = User(1054995036,'Donald','Herrera Vargas', Gender.MALE, '3012232219', '3149847223', 'jack2119hv@gmail.com', 'Chupnelohpts', 'Kr 26 # 47-15', 1.78, 78, True, Purpose, Rh.O_POSITIVE, Eps.SURA, date(1994, 3, 13), MedicalControl, Routine, Ranking, UserSchedule, EmployeeSchedule, RoutineHistory, Bill, MonthlyPay)
-# print("==========//==========//==========//==========//==========//==========//==========")
-# print(f"El nombre del usuario es {person1.getName()}\n Su apellido es {person1.getLastName()}\n"+
-#     f"Su genero es {person1.getGender().value}")
+    # getter para Employee
+    def getEmployee(self):
+        return self.employee
+
+    # getter && setter para MedicalControl
+    def getMedicalControl(self):
+        try:
+            return self.medicalControl
+        except:
+            pass
+
+    def setMedicalControl(self, __medicalControl:List[Any]):
+        try:
+            self.medicalControl = __medicalControl
+        except:
+            pass
+
+    # getter && setter para ScheduleUser
+    def getScheduleUser(self):
+        return self.scheduleUser
+
+    def setScheduleUser(self, __ScheduleUser:List[Any]):
+        self.scheduleUser = __ScheduleUser
+
+# getter && setters ENDS
+############################################################################################
+# Pendiente metodos de creación de rutina "addRoutine", "addMedicalControl" , "addSchedule"#
+############################################################################################
+
+person1 = User(1054995036, "Donald J.", "Herrera", Gender.MALE, "3012232219", "3148947223", "example.example.com", "123456789D", "Kr 26 # 47-15", 1.78, 78, True, Purpose.TONE_UP, Rh.O_POSITIVE, Eps.SURA, date(1994, 3, 13), Any, List[Any], Any, Any, List[Any], List[Any], List[Any], List[Any])
+print("==========//==========//==========//==========//==========//==========//==========")
+print(f"Nuevo Usuario Registrado \n",
+        f"Nombre: {person1.getName().upper()} {person1.getLastName().upper()} \n",
+        f"Genero: {person1.getGender().value.upper()}\n",
+        f"Numero de Contacto: {person1.getPhoneNumber()}\n",
+        f"Email: {person1.getEmailAddress().upper()}\n",
+        f"Fecha de Nacimiento: {person1.getBirthDate()}\n",
+        f"RH: {person1.getRh().value.upper()}\n",
+        f"EPS: {person1.getEps().value.upper()}\n",
+        f"Contacto Emergencia: {person1.getEmergencyContact()}")
