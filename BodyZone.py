a96738b762d9c4d96e661740056da5d1bffe872f
Jack21Ace <djherrera89639@umanizales.edu.co>
2022-04-10 21:10:56 -0500

    ...

--- BodyZone.py
+++
@@ -1,9 +0,0 @@
-from Enums import UpperBody, LowerBody, Conditioning
-class BodyZone:
-    # Constructor
-    def __init__(self, _upperBody:UpperBody, _lowerBody:LowerBody, _conditioning:Conditioning):
-
-        # Datos de Entrada
-        self._upperBody = _upperBody
-        self._lowerBody = _lowerBody
-        self._conditioning = _conditioning
